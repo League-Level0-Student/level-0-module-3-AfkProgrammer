3ffb2218ec5fa76f5f5eadc92a19766f254368c8
AfkProgrammer <vidriohans@gmail.com>
2020-08-01 18:28:38 +0000

    Almost done with Robot walk of fame

--- src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
+++ src/_02_loop_variables/_3_robot_walk_of_fame/WalkOfFame.java
@@ -5,11 +5,118 @@ import org.jointheleague.graphical.robot.Robot;
 
 public class WalkOfFame {
 	public static void main(String[] args) {
-		Robot rob = new Robot();
+		Robot rob = new Robot("mini");
+		rob.setX(50);
+		rob.setY(550);
+		rob.penDown();
+		rob.setPenColor(255,0,0);
+		rob.setSpeed(100);
 		
+		// Creating the star
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+		
+		rob.setX(100);
+		rob.setAngle(0);
+		
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+		
+		rob.setX(100);
+		rob.setAngle(0);
+		
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+
+		rob.setX(100);
+		rob.setAngle(0);
+		
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+
+		rob.setX(100);
+		rob.setAngle(0);
+		
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+
+		rob.setX(100);
+		rob.setAngle(0);
+		
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+
+		rob.setX(100);
+		rob.setAngle(0);
+		
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+
+		rob.setX(100);
+		rob.setAngle(0);
+		
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+		rob.turn(144);
+		rob.move(30);
+
 		// 1. Set the X position of the robot so that it starts on the left. 
 		// You also need to show the robot to see the result of this line.
-
+		
 		// 2. Make the robot draw a star shape. Hint: angle=144.
 
 		// 3. Set the length of each line in the star to 30.
@@ -17,11 +124,10 @@ public class WalkOfFame {
 		/** THE CHALLENGE: **/
 		/*
 		 * Make the robot draw a line of stars like this:
-		 * http://bit.ly/RobotWalk
+		 * ://bit.ly/RobotWalk
 		 * 
 		 * Hint: The distance between stars is 50.
 		 */
-
 	}
-
-}
+	
+	}
